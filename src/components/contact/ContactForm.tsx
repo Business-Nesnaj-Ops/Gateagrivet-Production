4a55ba6d45530954d7c3727e85975cb7ecf810ea
Business-Nesnaj-Ops <business.jansenbarabona@gmail.com>
2025-08-05 09:58:19 +0800

    Updated ContactForm.tsx

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -118,7 +118,7 @@ const ContactForm = () => {
                 id="phone"
                 name="phone"
                 type="tel"
-                placeholder="+1 (555) 123-4567"
+                placeholder="(+63) (0) 9064142340"
                 {...formik.getFieldProps('phone')}
                 className={`w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500 ${
                   formik.touched.phone && formik.errors.phone
