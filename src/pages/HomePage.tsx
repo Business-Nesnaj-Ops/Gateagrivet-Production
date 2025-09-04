fa93a6be096b759a16e27b20471a80c5ef0f1489
Business-Nesnaj-Ops <business.jansenbarabona@gmail.com>
2025-09-04 21:40:55 +0800

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -38,14 +38,14 @@ const HomePage = () => {
   <div className="grid grid-cols-2 gap-8 items-center justify-center max-w-md mx-auto">
     <div className="flex justify-center">
       <img 
-        src="/images/logo1.png" 
+        src="/logos/1.png" 
         alt="Logo 1" 
         className="h-16 object-contain"
       />
     </div>
     <div className="flex justify-center">
       <img 
-        src="/images/logo2.png" 
+        src="/logos/2.png" 
         alt="Logo 2" 
         className="h-16 object-contain"
       />
