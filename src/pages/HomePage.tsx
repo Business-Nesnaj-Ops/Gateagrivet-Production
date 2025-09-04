679e6e162d4044c4ecc9b7a39d0e11e2ce8f4d79
Business-Nesnaj-Ops <business.jansenbarabona@gmail.com>
2025-09-04 21:13:55 +0800

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,7 +21,7 @@ const HomePage = () => {
           <p className="text-lg text-gray-600 text-center max-w-3xl mx-auto mb-12">
              {/*We partner with the world's most respected brands to bring you premium quality products and exceptional service. Our commitment to excellence has made us a trusted name in the industry.*/}
           </p>
-          <PartnerLogos />
+          <img src="/product/01_Animal_Protein/POULTRY MEAL (HYRDROLYZED).jpg" alt="POULTRY MEAL (HYRDROLYZED)" class="w-full h-full object-contain">
         </div>
       </section>
       <section className="py-16 bg-white">
