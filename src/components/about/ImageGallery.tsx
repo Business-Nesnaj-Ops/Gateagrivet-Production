7433a8b5eb1cd6eb21c3e74823435a57035be66e
Business-Nesnaj-Ops <business.jansenbarabona@gmail.com>
2025-09-06 19:32:06 +0800

    Updated ImageGallery.tsx

--- src/components/about/ImageGallery.tsx
+++ src/components/about/ImageGallery.tsx
@@ -6,7 +6,7 @@ const galleryImages = [
   {
     id: 1,
     src: '/img/2025 LTO BAI IMPORTER.GATEAGRIVET INC._page-0001.jpg',
-    alt: '2025_LTO_BAI_IMPORTER_GATEAGRIVET_INC..png',
+    alt: '2025 LTO BAI IMPORTER.GATEAGRIVET INC._page-0001',
   },
   {
     id: 2,
